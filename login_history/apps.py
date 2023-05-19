b5960e240da3c832b415973f1154df6edcabe937
Farhad Hossain <farhadhossain0085@gmail.com>
2023-05-19 23:27:06 +0600

    added default_auto_field

--- login_history/apps.py
+++ login_history/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class LoginHistoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'login_history'
     verbose_name = "Login History"
